d69e9a3f667e20946b969f265765b92b4536bbd4
jgafnea <jgafnea@mail.com>
2024-05-05 04:16:54 -0500

    fix: update modules in tests to match code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,13 +2,13 @@ from unittest.mock import patch
 
 import pytest
 
-from libget.cli import main
+from libget.console import run
 
 
 def test_without_arguments():
     with pytest.raises(SystemExit) as e:
         with patch("sys.argv", [""]):
-            main()
+            run()
 
     assert e.type == SystemExit
     assert e.value.code == 2
