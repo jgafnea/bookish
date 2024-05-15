51324c7cd8e7fc959cf75d7c186cdc103785301b
jgafnea <jgafnea@mail.com>
2024-05-15 10:43:29 -0500

    feat: add progress bar

--- booksi/search.py
+++ booksi/search.py
@@ -1,6 +1,8 @@
 from libgen_api import LibgenSearch
 from pyshorteners import Shortener
+from rich import box
 from rich.console import Console
+from rich.progress import track
 from rich.table import Table
 
 
@@ -14,7 +16,8 @@ def search_books(query) -> list:
     filtered = search.search_title_filtered(query, lang_filter)
     results = [book for book in filtered if book["Extension"] in (file_filter)]
 
-    for book in results:
+    for book in track(results, description="Working..."):
+
         # Resolve links then shorten so they fit table
         resolved = search.resolve_download_links(book)["GET"]
         tinyurl = Shortener().tinyurl.short(resolved)
@@ -25,13 +28,14 @@ def search_books(query) -> list:
 
 def display_results(results):
     # https://rich.readthedocs.io/en/stable/appendix/colors.html
-    table = Table(title="Search Results")
-    table.add_column("Title", style="bright_cyan", justify="left")
-    table.add_column("Year", style="cyan", justify="left")
-    table.add_column("Author", style="bright_magenta", justify="left")
-    table.add_column("Ext", style="green", justify="left")
-    table.add_column("Size", style="bright_green", justify="right")
-    table.add_column("Link", style="bright_blue", justify="left")
+    table = Table(title="", box=box.SIMPLE_HEAD)
+
+    table.add_column("Title", style="bright_cyan")
+    table.add_column("Year", style="bright_magenta")
+    table.add_column("Author", style="yellow")
+    table.add_column("Ext", style="green")
+    table.add_column("Size", style="dim", justify="right")
+    table.add_column("Download", style="blue")
 
     for book in results:
         table.add_row(
