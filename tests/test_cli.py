2fa0f7ff838ce48eee1168d01b517738bf75b05b
jgafnea <jgafnea@mail.com>
2024-05-15 13:48:27 -0500

    fix: update tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,13 +2,13 @@ from unittest.mock import patch
 
 import pytest
 
-from src.console import run
+from src.runner import main
 
 
 def test_without_arguments():
     with pytest.raises(SystemExit) as e:
         with patch("sys.argv", [""]):
-            run()
+            main()
 
     assert e.type == SystemExit
     assert e.value.code == 2
